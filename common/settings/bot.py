77b4cfd849a7be0fc42a25a878352da11e66a476
Alex Krantz <alex@krantz.dev>
2021-03-13 00:06:00 -0800

    Add ability to register for external services

--- common/settings/bot.py
+++ common/settings/bot.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class BotSettings(BaseModel):
     log_file: str = "discord.log"
+    registration_config: str = "registration.json"
 
     class Config:
         env_prefix = "BOT_"
