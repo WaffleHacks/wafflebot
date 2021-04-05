e042cb8f49159bc5e99b9ddd7b80d68f90452a09
Alex Krantz <alex@krantz.dev>
2021-04-04 17:28:55 -0700

    Synchronize response models with databae models

--- api/tickets/models/tickets.py
+++ api/tickets/models/tickets.py
@@ -1,13 +1,15 @@
+from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional
 
 
 class TicketResponse(BaseModel):
     id: int
-    category_id: int
+    category_id: Optional[int]
     creator_id: int
     is_open: bool
     reason: Optional[str]
+    created_at: Optional[datetime]
 
     class Config:
         orm_mode = True
