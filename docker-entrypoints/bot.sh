87315c0eb9d7439489611770352266660dc068bb
Bisman18 <44409816+Bisman18@users.noreply.github.com>
2022-05-18 13:18:49 +0530

    Removed docker-entrypoints

--- docker-entrypoints/bot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python manage.py migrations run
-exec python manage.py run bot
--- docker-entrypoints/web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python manage.py migrations run
-exec gunicorn -k uvicorn.workers.UvicornWorker \
-  --access-logfile - \
-  --bind '[::]:8000' \
-  --worker-tmp-dir /dev/shm \
-  --workers "${GUNICORN_WORKERS:-3}" \
-  api.main:app
