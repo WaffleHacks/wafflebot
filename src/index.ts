9de4c8fa1ee6fd51695649f55fe1d58378f9ade9
Alex Krantz <alex@krantz.dev>
2023-06-04 11:28:35 -0700

    Add discord bot using sapphire

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import CONFIG from '@lib/config';
+import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
 
-console.log(CONFIG);
+import CONFIG from '@lib/config';
+import logger from '@lib/logger';
+
+const client = new SapphireClient({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  logger: {
+    level: process.env.NODE_ENV === 'development' ? LogLevel.Debug : LogLevel.Info,
+    instance: logger.child('discord'),
+  },
+  typing: true,
+});
+
+client.login(CONFIG.token);
--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,11 +1,91 @@
-import { createLogger, format, transports } from 'winston';
+import { ILogger, LogLevel } from '@sapphire/framework';
+import { Logger as WinstonLogger, createLogger, format, transports } from 'winston';
 
-const logger = createLogger({
-  transports: [new transports.Console()],
-  format: format.combine(
-    format.timestamp(),
-    process.env.NODE_ENV === 'production' ? format.json() : format.combine(format.colorize(), format.simple()),
-  ),
-});
+export class Logger implements ILogger {
+  readonly logger: WinstonLogger;
+
+  constructor(namespace?: string, parent?: WinstonLogger) {
+    if (parent) {
+      this.logger = parent.child({ namespace });
+    } else {
+      this.logger = createLogger({
+        transports: [new transports.Console()],
+        format: format.combine(
+          format.timestamp(),
+          format.errors({ stack: true }),
+          process.env.NODE_ENV === 'production' ? format.json() : format.combine(format.colorize(), format.simple()),
+        ),
+        handleExceptions: true,
+        handleRejections: true,
+      });
+    }
+  }
+
+  public child(namespace: string): Logger {
+    return new Logger(namespace, this.logger);
+  }
+
+  has(level: LogLevel): boolean {
+    return this.logger.isLevelEnabled(this.logLevelName(level));
+  }
+
+  trace(...values: readonly unknown[]): void {
+    this.write(LogLevel.Trace, ...values);
+  }
+
+  debug(...values: readonly unknown[]): void {
+    this.write(LogLevel.Debug, ...values);
+  }
+
+  info(...values: readonly unknown[]): void {
+    this.write(LogLevel.Info, ...values);
+  }
+
+  warn(...values: readonly unknown[]): void {
+    this.write(LogLevel.Warn, ...values);
+  }
+
+  error(...values: readonly unknown[]): void {
+    this.write(LogLevel.Error, ...values);
+  }
+
+  fatal(...values: readonly unknown[]): void {
+    this.write(LogLevel.Fatal, ...values);
+  }
+
+  write(level: LogLevel, ...values: readonly unknown[]): void {
+    const levelName = this.logLevelName(level);
+
+    if (values.length >= 1) {
+      if (typeof values[0] === 'string') {
+        const [message, ...meta] = values;
+        this.logger.log(levelName, message, ...meta);
+      } else {
+        this.logger.log(levelName, values);
+      }
+    }
+  }
+
+  private logLevelName(level: LogLevel): string {
+    switch (level) {
+      case LogLevel.Trace:
+        return 'trace';
+      case LogLevel.Debug:
+        return 'debug';
+      case LogLevel.Info:
+        return 'info';
+      case LogLevel.Warn:
+        return 'warn';
+      case LogLevel.Error:
+        return 'error';
+      case LogLevel.Fatal:
+        return 'fatal';
+      default:
+        return 'unknown';
+    }
+  }
+}
+
+const logger = new Logger();
 
 export default logger;
