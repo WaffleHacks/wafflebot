a9edb5a13a9debd3db64a19973087966eb3dcc99
Alex Krantz <alex@krantz.dev>
2023-06-04 17:11:23 -0700

    Add verify button handler

---
+++ src/interaction-handlers/verify.ts
@@ -0,0 +1,30 @@
+import { InteractionHandler, InteractionHandlerTypes, Piece } from '@sapphire/framework';
+import { type ButtonInteraction, TextInputStyle } from 'discord.js';
+import { ActionRowBuilder, ModalBuilder, TextInputBuilder } from 'discord.js';
+
+export class VerifyButtonHandler extends InteractionHandler {
+  constructor(context: Piece.Context, options: InteractionHandler.Options) {
+    super(context, { ...options, interactionHandlerType: InteractionHandlerTypes.Button });
+  }
+
+  public async run(interaction: ButtonInteraction) {
+    const modal = new ModalBuilder()
+      .setCustomId('verifier')
+      .setTitle('Get verified!')
+      .addComponents(
+        new ActionRowBuilder<TextInputBuilder>().addComponents(
+          new TextInputBuilder()
+            .setCustomId('email')
+            .setLabel("What's the email you applied with?")
+            .setRequired(true)
+            .setStyle(TextInputStyle.Short),
+        ),
+      );
+    await interaction.showModal(modal);
+  }
+
+  public override parse(interaction: ButtonInteraction) {
+    if (interaction.customId === 'verify') return this.some();
+    else return this.none();
+  }
+}
