258595f0543a3ee7921533cf67f5faf4c0e9df4d
Alex Krantz <alex@krantz.dev>
2023-06-07 16:50:17 -0700

    Bundle using tsup instead of tsc

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup';
+
+// defined folders from .sapphirerc.json
+const sapphireFolders = ['arguments', 'commands', 'interaction-handlers', 'listeners', 'preconditions'].map(
+  (folder) => `src/${folder}/*.ts`,
+);
+
+export default defineConfig({
+  clean: true,
+  bundle: true,
+  dts: false,
+  entry: ['src/index.ts', ...sapphireFolders],
+  format: ['cjs'],
+  minify: false,
+  tsconfig: 'tsconfig.json',
+  target: 'es2020',
+  splitting: true,
+  skipNodeModulesBundle: true,
+  sourcemap: true,
+  shims: false,
+  keepNames: true,
+});
