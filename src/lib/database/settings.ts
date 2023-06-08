0cd658b6d2f2aacdc8e402cc81fa07d37179771f
Alex Krantz <alex@krantz.dev>
2023-06-07 18:32:43 -0700

    Add a settings cache

--- src/lib/database/settings.ts
+++ src/lib/database/settings.ts
@@ -1,3 +1,5 @@
+import NodeCache from 'node-cache';
+
 import { prisma } from './client';
 
 interface VerificationMessage {
@@ -7,15 +9,23 @@ interface VerificationMessage {
   footerId: string;
 }
 
+// Cache settings for 6 hours by default
+const cache = new NodeCache({ stdTTL: 6 * 60 * 60 });
+
 export class Settings {
   /**
    * Get the verification message information
    */
   public static async getVerificationMessage(): Promise<VerificationMessage | null> {
+    const cached = cache.get<VerificationMessage>('verification-message');
+    if (cached) return cached;
+
     const setting = await prisma.setting.findFirst({ where: { key: 'VERIFICATION_MESSAGE' } });
     if (setting === null) return null;
 
-    return JSON.parse(setting.value);
+    const deserialized = JSON.parse(setting.value);
+    cache.set('verification-message', deserialized);
+    return deserialized;
   }
 
   /**
@@ -29,15 +39,20 @@ export class Settings {
       update: { value: serialized },
       where: { key: 'VERIFICATION_MESSAGE' },
     });
+    cache.set('verification-message', message);
   }
 
   /**
    * Get the ID of the participant role
    */
   public static async getParticipantRole(): Promise<string | null> {
+    const cached = cache.get<string>('participant-role');
+    if (cached) return cached;
+
     const setting = await prisma.setting.findFirst({ where: { key: 'PARTICIPANT_ROLE' } });
     if (setting === null) return null;
 
+    cache.set('participant-role', setting.value);
     return setting.value;
   }
 
@@ -51,5 +66,6 @@ export class Settings {
       update: { value: id },
       where: { key: 'PARTICIPANT_ROLE' },
     });
+    cache.set('participant-role', id);
   }
 }
