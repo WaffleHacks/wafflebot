7a7c674546743e98c62f915068654697c4ae8380
Alex Krantz <alex@krantz.dev>
2023-06-07 23:32:52 -0700

    Add application portal lookup api

--- src/lib/portal.ts
+++ src/lib/portal.ts
@@ -7,6 +7,12 @@ export enum Status {
 }
 
 interface StatusResponse {
+  id: number;
+  status: Status;
+}
+
+export interface ApplicationStatus {
+  id?: number;
   status: Status | null;
 }
 
@@ -14,16 +20,39 @@ interface StatusResponse {
  * Lookup the status of an application by the participant's email
  * @param email
  */
-export async function lookupApplicationStatusByEmail(email: string): Promise<Status | null> {
-  const response = await fetch(
-    `${APPLICATION_PORTAL_URL}/integrations/wafflebot/status?email=${encodeURIComponent(email)}`,
-    {
-      headers: {
-        Authorization: `Bearer ${APPLICATION_PORTAL_TOKEN}`,
-      },
-    },
-  );
-  const data: StatusResponse = await response.json();
+export async function lookupApplicationStatusByEmail(email: string): Promise<ApplicationStatus> {
+  const data: StatusResponse | null = await request(`status?email=${encodeURIComponent(email)}`);
+  if (data === null) return { status: null };
+  else return data;
+}
 
-  return data.status;
+export interface UserInfo {
+  first_name: string;
+  last_name: string;
+  email: string;
+  link: string;
+}
+
+/**
+ * Lookup a participant's information by their email
+ * @param email
+ */
+export const lookupParticipantByEmail = async (email: string): Promise<UserInfo | null> =>
+  request(`lookup?email=${encodeURIComponent(email)}`);
+
+/**
+ * Lookup a participant's information by their application portal ID
+ * @param id
+ */
+export const lookupParticipantByID = async (id: number): Promise<UserInfo | null> => request(`lookup?id=${id}`);
+
+async function request<T>(pathAndQuery: string): Promise<T> {
+  const response = await fetch(`${APPLICATION_PORTAL_URL}/integrations/wafflebot/${pathAndQuery}`, {
+    headers: {
+      Authorization: `Bearer ${APPLICATION_PORTAL_TOKEN}`,
+    },
+  });
+  if (response.status !== 200) throw new Error(`unexpected response: ${await response.text()} (${response.status})`);
+
+  return await response.json();
 }
