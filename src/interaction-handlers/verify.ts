21c9faef7b81d8892a0d5ea8a9e6cf8da6f46704
Alex Krantz <alex@krantz.dev>
2023-06-07 18:51:21 -0700

    Check that the verification is setup properly before proceeding

--- src/interaction-handlers/verify.ts
+++ src/interaction-handlers/verify.ts
@@ -1,12 +1,22 @@
 import { InteractionHandler, InteractionHandlerTypes, Piece } from '@sapphire/framework';
 import { ActionRowBuilder, type ButtonInteraction, ModalBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 
+import { Settings } from '@lib/database';
+
 export class VerifyButtonHandler extends InteractionHandler {
   constructor(context: Piece.Context, options: InteractionHandler.Options) {
     super(context, { ...options, interactionHandlerType: InteractionHandlerTypes.Button });
   }
 
   public async run(interaction: ButtonInteraction) {
+    if (await this.verificationNotSetup(interaction)) {
+      await interaction.reply({
+        ephemeral: true,
+        content: ':x: Verification is not setup properly, please contact an organizer.',
+      });
+      return;
+    }
+
     const modal = new ModalBuilder()
       .setCustomId('verifier')
       .setTitle('Get verified!')
@@ -25,5 +35,16 @@ export class VerifyButtonHandler extends InteractionHandler {
   public override parse(interaction: ButtonInteraction) {
     if (interaction.customId === 'verify') return this.some();
     else return this.none();
+  }
+
+  private async verificationNotSetup(interaction: ButtonInteraction): Promise<boolean> {
+    const participantRoleId = await Settings.getParticipantRole();
+    if (participantRoleId === null) return true;
+
+    const participantRole = await interaction.guild?.roles.fetch(participantRoleId);
+    if (participantRole === null || participantRole == undefined) return true;
+
+    const verification = await Settings.getVerificationMessage();
+    return verification === null;
   }
 }
