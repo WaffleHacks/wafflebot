3136dd6590b2a00f52a7b99511c45e471796b10c
Alex Krantz <alex@krantz.dev>
2023-06-10 16:50:20 -0700

    Allow deleting a single command

--- scripts/clear-commands.mjs
+++ scripts/clear-commands.mjs
@@ -11,7 +11,8 @@ program
       .env('DISCORD_APPLICATION_ID')
       .makeOptionMandatory(true),
   )
-  .addOption(new Option('-g, --guild <id>', 'guild ID to restrict to').env('DISCORD_GUILD_ID'));
+  .addOption(new Option('-g, --guild <id>', 'guild ID to restrict to').env('DISCORD_GUILD_ID'))
+  .addOption(new Option('-c, --command <name>', 'command name to delete'));
 
 program.parse();
 const options = program.opts();
@@ -30,9 +31,10 @@ if (options.guild !== undefined && options.guild.length > 1) {
     const rest = new REST({ version: '10' }).setToken(options.token);
     const commands = await rest.get(basePath);
 
-    console.log(`Deleting ${commands.length} commands... This could take a while.`);
+    const toDelete = commands.filter((c) => options.command === undefined || c.name === options.command);
+    console.log(`Deleting ${toDelete.length} commands... This could take a while.`);
 
-    const tasks = commands.map((c) => rest.delete(`${basePath}/${c.id}`));
+    const tasks = toDelete.map((c) => rest.delete(`${basePath}/${c.id}`));
     await Promise.all(tasks);
 
     console.log('Done!');
